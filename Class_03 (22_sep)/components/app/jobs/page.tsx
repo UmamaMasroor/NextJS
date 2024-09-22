fd8cf1c8a04809a668dca778a75db5b503d3dfab
MohammadUmar04 <umamafatima319@gmail.com>
2024-09-22 11:32:13 +0500

    components and routing

--- Class_03 (22_sep)/components/app/jobs/page.tsx
+++ Class_03 (22_sep)/components/app/jobs/page.tsx
@@ -1,7 +1,8 @@
-
+import Header from "../components/header"
 export default function Jobs() {
     return (
      <div> 
+      <Header/>
       <h1>Find Jobs</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium voluptatem neque illo reprehenderit tenetur inventore vitae molestias vero sequi ducimus, repellendus sunt velit voluptates possimus ad itaque incidunt assumenda, esse hic id a dolor veritatis. Inventore iste a illo maiores facere eligendi maxime exercitationem. Nihil ducimus laudantium quisquam dolor hic.</p>
       </div>
