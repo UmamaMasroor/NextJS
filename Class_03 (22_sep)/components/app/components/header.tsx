610f5e7aa08305871bb024294be34321fdfad7c6
MohammadUmar04 <umamafatima319@gmail.com>
2024-09-22 11:12:44 +0500

    components and routing

--- Class_03 (22_sep)/components/app/components/header.tsx
+++ Class_03 (22_sep)/components/app/components/header.tsx
@@ -3,7 +3,7 @@ export default function header(){
     return (
         <div className="header">
            <ul className="header-links">
-           <li>Home</li>
+           <Link href={"/"}><li>Home</li></Link>
            <Link href={"/about-us"}><li>About Us</li></Link>
            <Link href={"/jobs"} ><li>Jobs</li></Link>
             <Link href={"/contact-us"}><li>Contact Us</li></Link>
